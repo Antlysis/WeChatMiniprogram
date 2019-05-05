cbabc0af044eda8e9542a6920e802c55f0597e90
hzchin <chinhanzhong@gmail.com>
2019-05-05 22:32:20 +0800

    add all thing

---
+++ GraceB/miniprogram/app.js
@@ -0,0 +1,16 @@
+//app.js
+//AppSecret(小程序密钥) a771a7467c9a4874cf8afc1709887f68
+App({
+  onLaunch: function () {
+    
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        traceUser: true,
+      })
+    }
+
+    this.globalData = {}
+  }
+})
